bcc15be3a179d884c7923fbe607d50ab891877d1
giacomo1235 <giacomo.passariello@gmail.com>
2022-04-08 22:23:28 +0200

    modifiche

--- snack.php
+++ snack.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Punteggi Basket</title>
+    <title>Snacks</title>
 </head>
 <body>
+    <!-- SNACK 1 -->
+    <h1>Punteggi Basket</h1>
     <ul> <?php
             $array_partite = [
                 [
@@ -48,6 +50,33 @@
                 </li><?php
             }?>
         </ul>
+<!-- / SNACK 1 -->
+<!-- SNACK 2 -->
+<h1>E-Mail</h1>
+<form action="" method="GET">
+       <label for="name">Nome:</label>
+       <input type="text" name="name" id="name">
+       <label for="email">Email:</label>
+       <input type="text" name="email" id="email">
+       <label for="age">Età:</label>
+       <input type="text" name="number" id="number">
+       <button>INVIO</button>
+   </form>
+
+   <?php
+       if (isset($_GET['email']) && isset($_GET['name']) && isset($_GET['number'])) {
+        $email = $_GET['email'];
+        $name = $_GET['name'];
+        $number = $_GET['number'];
+        $pos_chiocciola = strpos($email, '@');
+    
+            if ($pos_chiocciola !== false && strpos($email, '.', $pos_chiocciola) !== false && strlen($name) < 3 !== true &&  is_numeric($number) !== false ) {
+                echo ucwords('accesso riuscito');
+            } else {
+                echo ucwords('accesso negato');
+            };
+        };
+   ?>
 
         
     
